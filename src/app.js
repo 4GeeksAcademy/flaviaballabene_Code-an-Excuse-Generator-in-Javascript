f263dab42a41828f7976a2e3c4f6a7125696ee4a
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-02-02 03:54:33 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+    const who = ['John', 'Alice', 'Bob', 'Emma'];
+    const action = ['is eating', 'is reading', 'is playing', 'is coding'];
+    const what = ['a pizza', 'a book', 'a game', 'JavaScript'];
+    const when = ['right now', 'at noon', 'this evening', 'tomorrow'];
+
+    function shuffleArray(array) {
+      const shuffledIndex = Math.floor(Math.random() * array.length);
+      const shuffledElement = array[shuffledIndex];
+      return shuffledElement;
+    }
+    
+    function generateRandomSentence () {
+      const shuffledWho = shuffleArray(who);
+      const shuffledAction = shuffleArray(action);
+      const shuffledWhat = shuffleArray(what);
+      const shuffledWhen = shuffleArray(when);
+
+      const randomSentence = `${shuffledWho} ${shuffledAction} ${shuffledWhat} ${shuffledWhen}`;
+      return {
+        sentence: randomSentence,
+        hasPizza: shuffledWhat === 'a pizza',
+        hasBook: shuffledWhat === 'a book',
+        hasGame: shuffledWhat === 'a game',
+        hasJavaScript: shuffledWhat === 'JavaScript',
+      };
+  };
+
+  window.onload = function () {
+    const excuseElement = document.getElementById('excuse');
+  const emojiElement = document.getElementById('emoji');
+
+  const { sentence, hasPizza, hasBook, hasGame, hasJavaScript } = generateRandomSentence();
+
+  excuseElement.textContent = sentence;
+
+  emojiElement.textContent = '';
+
+  if (hasPizza) {
+    emojiElement.textContent += '🍕';
+  }
+
+  if (hasBook) {
+    emojiElement.textContent += '📚';
+  }
+
+  if (hasGame) {
+    emojiElement.textContent += '🎮';
+  }
+
+  if (hasJavaScript) {
+    emojiElement.textContent += '💻';
+  }
 };
